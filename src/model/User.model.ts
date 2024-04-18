e55ddabf1bbcc8e7bc5c44321a4ae83af3a913cc
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-04-18 20:31:27 +0600

    User Model created

--- src/model/User.model.ts
+++ src/model/User.model.ts
@@ -66,3 +66,9 @@ const UserSchema: Schema<User> = new Schema({
   },
   messages: [MessageSchema],
 });
+
+const UserModel =
+  (mongoose.models.User as mongoose.Model<User>) ||
+  mongoose.model<User>("User", UserSchema);
+
+export default UserModel;
